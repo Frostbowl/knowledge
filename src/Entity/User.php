e24482886cd3ecd6e7928a7c28e1f6c4e1bbcaf7
Frostbowl <masclet.c@gmail.com>
2024-08-21 01:11:53 +0400

    creation entity user + login & register

---
+++ src/Entity/User.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'Cette adresse email est déjà utilisée.')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    #[ORM\Column(length:255)]
+    private ?string $name = null;
+
+
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = ['ROLE_USER'];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $is_verified = false;
+
+    #[ORM\Column(type: 'string', length:255, nullable: true)]
+    private $confirmationToken;
+    
+
+    /////////////////////////////////////////////////////////////////Getters & Setters////////////////////////////////////////////////////////////////
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    } 
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        if (!in_array('ROLE_USER', $roles, true)){
+            $roles[]= 'ROLE_USER';
+        }
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        if (!in_array('ROLE_USER', $roles, true)){
+            $roles[]='ROLE_USER';
+        }
+
+        $this->roles = array_unique($roles);
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getIsVerified(): ?bool
+    {
+        return $this->is_verified;
+    }
+
+    public function setIsVerified(bool $is_verified): static
+    {
+        $this->is_verified = $is_verified;
+
+        return $this;
+    }
+
+    public function getConfirmationToken(): ?string
+    {
+        return $this->confirmationToken;
+    }
+
+    public function setConfirmationToken(?string $confirmationToken): self
+    {
+        $this->confirmationToken = $confirmationToken;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormError;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('name', TextType::class, [
+                'constraints'=>[
+                    new NotBlank([
+                        'message'=>'Entrez votre nom et votre prénom',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'mapped'=>false,
+                'attre'=>['autocomplete'=> 'new-password'],
+                'constraints'=>[
+                    new NotBlank([
+                        'message'=> 'Entrez un mot de passe',
+                    ]),
+                    new Length([
+                        'min'=>6,
+                        'minMessage'=>'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max'=>4096,
+                    ]),
+                ],
+            ])
+            ->add('plainPasswordConfirm', PasswordType::class, [
+                'mapped'=>false,
+                'att'=>['autocomplete'=>'new-password'],
+                'constraints'=>[
+                    new NotBlank([
+                        'message'=>'Confirmez votre mot de passe',
+                    ]),
+                ],
+            ])
+            ->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event){
+                $form = $event->getForm();
+                $date = $event->getData();
+                $plainPassword = $form->get('plainPassword')->getData();
+                $plainPasswordConfirm = $form->get('plainPasswordConfirm')->getData();
+
+                if($plainPassword !== $plainPasswordConfirm){
+                    $form->get('plainPasswordConfirm')->addError(new FormError('Les mots de passe ne correspondent pas'));
+                }
+            })
+        ;
+    }   
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'validation_groups' => ['registration']
+        ]);
+    }
+}
