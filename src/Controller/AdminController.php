ec41bf842e07bc570c6d04d27abcf8c781557375
Frostbowl <masclet.c@gmail.com>
2024-08-21 22:39:41 +0400

    creation controller theme

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Theme;
+use App\Form\ThemeType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_ADMIN')]
@@ -15,6 +20,36 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/admin/theme', name: "admin_create_theme")]
+    public function createTheme(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $theme = new Theme();
+        $form = $this->createForm(ThemeType::class, $theme);
+
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted() && $form->isValid()){
+
+            $theme->setCreatedAt(new \DateTimeImmutable());
+            $theme->setCreatedBy($this->getUser());
+            $user = $this->getUser();
+            if ($user instanceof UserInterface){
+                $theme->setCreatedBy($user);
+            }
+
+            $entityManager->persist($theme);
+            $entityManager->flush();
+
+            $this->addFlash('succes', 'Nouveau theme créé');
+
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('admin/create_theme.html.twig', [
+            'form'=> $form->createView(),
         ]);
     }
 }
--- src/Form/ThemeType.php
+++ src/Form/ThemeType.php
@@ -8,28 +8,19 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ThemeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('created_at', null, [
-                'widget' => 'single_text'
-            ])
-            ->add('updated_at', null, [
-                'widget' => 'single_text'
-            ])
-            ->add('created_by', EntityType::class, [
-                'class' => User::class,
-'choice_label' => 'id',
-            ])
-            ->add('updated_by', EntityType::class, [
-                'class' => User::class,
-'choice_label' => 'id',
-            ])
-        ;
+            ->add('name', TextType::class, [
+                'label'=>'Nom du thème',
+                'attr'=>[
+                    'placeholder'=>'Entrez le nom du thème',
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
