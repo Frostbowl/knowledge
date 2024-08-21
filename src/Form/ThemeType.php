368ef70da517a4e3ee7aa9c0136e5f816c6c7e4d
Frostbowl <masclet.c@gmail.com>
2024-08-21 21:21:10 +0400

    creation route admin

---
+++ src/Form/ThemeType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Theme;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ThemeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('created_at', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('updated_at', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('created_by', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+            ->add('updated_by', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Theme::class,
+        ]);
+    }
+}
