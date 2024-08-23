60e25a80bd03a6af620b9dca3016215abdbd9e58
Frostbowl <masclet.c@gmail.com>
2024-08-23 20:57:01 +0400

    MaJ CSS

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Cursus;
+use App\Entity\Theme;
+use App\Repository\CursusRepository;
+use App\Repository\LessonsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,6 +19,37 @@ class FormationController extends AbstractController
         $themes = $themeRepository->findAll();
         return $this->render('formation/index.html.twig', [
             'themes'=>$themes,
+        ]);
+    }
+
+    #[Route('/formation/{id}/cursus', name:'app_theme_cursus')]
+    public function cursus(Theme $theme, CursusRepository $cursusRepository):Response
+    {
+        $cursus = $cursusRepository->findBy(['theme'=>$theme]);
+        return $this->render('formation/cursus.html.twig', [
+            'theme'=>$theme,
+            'cursus'=>$cursus,
+        ]);
+    }
+
+    #[Route('/formation/{themeId}/cursus/{cursusId}/lesson', name:'app_cursus_lesson')]
+    public function lesson (int $cursusId, CursusRepository $cursusRepository ,LessonsRepository $lessonsRepository): Response
+    {
+        $cursus = $cursusRepository->find($cursusId);
+        $lesson = $lessonsRepository->findBy(['cursus'=>$cursus]);
+        return $this->render('formation/lesson.html.twig',[
+            'cursus'=>$cursus,
+            'lessons'=>$lesson
+        ]);
+    }
+
+    #[Route('/formation/{themeId]/cursus/{cursusId}/lesson/{lessonId}', name:'app_lesson')]
+    public function lessonDetail(int $lessonId, LessonsRepository $lessonsRepository): Response
+    {
+        $lesson = $lessonsRepository->find($lessonId);
+
+        return $this->render('formation/lesson_detail.html.twig', [
+            'lesson'=> $lesson,
         ]);
     }
 }
